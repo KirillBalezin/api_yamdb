cc80c898ab50e44562d1ec0c501ad65399272a48
l8beOne <klimentiy.proletskiy@gmail.com>
2023-03-14 22:47:26 +0600

    Добавил модели произведений, жанров, категорий

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    year = models.IntegerField()
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genres,
+        on_delete=models.SET_NULL,
+    )
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.SET_NULL,
+    )
+
+    def __str__(self):
+        return self.name
