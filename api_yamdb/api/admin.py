e45a8bdf5a1fa7cca007c803e910a5f61154ce98
Kirill Balezin <kiruha09890@mail.ru>
2023-03-15 21:04:41 +0300

    Update admin.py

--- api_yamdb/api/admin.py
+++ api_yamdb/api/admin.py
@@ -1,15 +1,1 @@
-from django.contrib import admin
 
-from reviews.models import Categories, Genres, Titles
-
-
-class TitlesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'year', 'description', 'category',)
-    search_fields = ('name',)
-    list_filter = ('year',)
-    empty_value_display = '-пусто-'
-
-
-admin.site.register(Titles, TitlesAdmin)
-admin.site.register(Categories)
-admin.site.register(Genres)
