cd4b8c37a9e0251d7a4475bc1ae99272179cd2ac
l8beOne <klimentiy.proletskiy@gmail.com>
2023-03-15 23:13:19 +0600

    Подправил модель

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -3,33 +3,65 @@ from django.db import models
 
 
 class Genres(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True, max_length=50)
+    name = models.CharField(
+        max_length=256,
+        verbose_name="Название жанра"
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+        verbose_name="Slug жанра"
+    )
+
+    class Meta:
+        default_related_name = 'genres'
 
     def __str__(self):
         return self.name
 
 
 class Categories(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(unique=True, max_length=50)
+    name = models.CharField(
+        max_length=256,
+        verbose_name="Название категории"
+    )
+    slug = models.SlugField(
+        unique=True,
+        max_length=50,
+        verbose_name="Slug категории"
+    )
+
+    class Meta:
+        default_related_name = 'categories'
 
     def __str__(self):
         return self.name
 
 
 class Titles(models.Model):
-    name = models.CharField(max_length=256)
-    year = models.IntegerField()
-    description = models.TextField()
+    name = models.CharField(
+        max_length=256,
+        verbose_name="Название произведения"
+    )
+    year = models.IntegerField(
+        verbose_name="Год выпуска"
+    )
+    description = models.TextField(
+        verbose_name="Описание"
+    )
     genre = models.ManyToManyField(
         Genres,
-        on_delete=models.SET_NULL,
+        verbose_name="Жанр"
     )
     category = models.ForeignKey(
         Categories,
+        null=True,
         on_delete=models.SET_NULL,
+        verbose_name="Категория"
     )
+
+    class Meta:
+        default_related_name = 'titles'
 
     def __str__(self):
         return self.name
