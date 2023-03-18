e4bc6d27787b96e96cde9cdd7cd06a06caaf31bd
Кирилл Балезин <Kirill.Balezin@yandex.ru>
2023-03-18 10:49:36 +0300

    маленечеко исправил views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, permissions
 
-from reviews.models import Title
+from reviews.models import Title, Review
 from .serializers import ReviewSerializer, CommentSerializer
 
 
@@ -27,13 +27,17 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = ...
 
     def get_review(self):
-        return get_object_or_404(Title, pk=self.kwargs.get('review_id'))
+        return get_object_or_404(
+            Review,
+            pk=self.kwargs.get('review_id'),
+            title__id=self.kwargs.get('title_id')
+        )
 
     def get_queryset(self):
-        return self.get_title().comments.all()
+        return self.get_review().comments.all()
 
     def perform_create(self, serializer):
         serializer.save(
             author=self.request.user,
-            title=self.get_review()
+            review=self.get_review()
         )
