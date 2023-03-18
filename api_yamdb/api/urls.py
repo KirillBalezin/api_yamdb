41d0fdeab9c94ccab548d961f9aff1295fc28419
Кирилл Балезин <Kirill.Balezin@yandex.ru>
2023-03-18 15:34:40 +0300

    первые исправления

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -29,7 +29,7 @@ router_v1.register(
     basename='reviews'
 )
 router_v1.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<title_id>\d+)/comments',
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
